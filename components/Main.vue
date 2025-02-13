269a316aa1c346742592ef4566c037dd0face5fe
Khromov <hromov@price.ru>
2025-02-13 15:32:06 +0100

    init

---
+++ components/Main.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+      Starts
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
